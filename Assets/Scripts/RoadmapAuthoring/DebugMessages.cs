9d45da9be431dc39804371f5d4adf8f1d9080291
Shariff Faleel <shariff.mfa@outlook.com>
2023-10-11 17:01:51 -0700

    Adding DebugMessages

---
+++ Assets/Scripts/RoadmapAuthoring/DebugMessages.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace ubc.ok.ovilab.roadmap
+{
+    public class DebugMessages : Singleton<DebugMessages>
+    {
+        public int maxMessages = 5;
+        public bool logToDebugLog = true;
+
+        private TextMeshProUGUI debugText;
+
+        private void Start()
+        {
+            if (debugText == null)
+            {
+                debugText = GetComponent<TextMeshProUGUI>();
+            }
+        }
+
+        public void LogToDebugText(string message)
+        {
+            if (logToDebugLog)
+            {
+                Debug.Log($"[DEBUG_MSG] {message}");
+            }
+
+            string[] lines = debugText.text.Split('\n');
+
+            debugText.text = $"{message}\n" + string.Join("\n", lines.Take(maxMessages));
+        }
+
+        public void ClearDebugText()
+        {
+            debugText.text = string.Empty;
+        }
+    }
+}
